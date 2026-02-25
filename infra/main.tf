1b0d96618e3c9fbd1eb46bdc4cb84aca181139e7
gmezan <meza.gustavo@pucp.edu.pe>
2026-02-25 00:21:00 -0500

    infra: provision Azure OpenAI

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,18 @@
-data "azurerm_resource_group" "current" {
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "rg" {
   name = "rg-ai-engineer-challenge"
+}
+
+resource "random_pet" "aoai" {
+  length = 2
+
+}
+
+resource "azurerm_cognitive_account" "aoai" {
+  name                = "aoai${random_pet.aoai.id}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  kind                = "OpenAI"
+  sku_name            = "S0"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group_name" {
-  value = data.azurerm_resource_group.current.name
+  value = data.azurerm_resource_group.rg.name
 }
 
 output "resource_group_location" {
-  value = data.azurerm_resource_group.current.location
+  value = data.azurerm_resource_group.rg.location
 }
