342c96db71ad8099ac19d4e6a604fabfbbe915f0
gmezan <meza.gustavo@pucp.edu.pe>
2026-02-25 22:37:16 -0500

    infra: deploy ai search and cosmos

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,11 @@ resource "random_pet" "aoai" {
   length = 2
 }
 
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 resource "azurerm_cognitive_account" "aoai" {
   name                = "aoai-${random_pet.aoai.id}"
   location            = data.azurerm_resource_group.rg.location
@@ -30,4 +35,41 @@ resource "azurerm_cognitive_deployment" "model_1" {
     name     = "Standard"
     capacity = 4
   }
+}
+
+resource "azurerm_search_service" "aisearch" {
+  name                = "aisearch-${random_pet.aoai.id}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  local_authentication_enabled = true
+}
+
+resource "azurerm_cosmosdb_account" "cosmos_account" {
+  name                = "coac-${random_pet.aoai.id}-${random_integer.ri.result}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  automatic_failover_enabled = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = data.azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "sql_db" {
+  name                = "codb-${random_pet.aoai.id}-${random_integer.ri.result}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.cosmos_account.name
+  # You can optionally define throughput here at the database level
 }
